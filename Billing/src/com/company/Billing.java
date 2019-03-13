e0e1f7bdd603bec6533d0dd78ec8c7fe3bbaed11
GharehzadehAli <gharehzadehali@gmail.com>
2019-03-13 14:10:40 +0330

    Last version of Billing

--- Billing/src/com/company/Billing.java
+++ Billing/src/com/company/Billing.java
@@ -4,6 +4,7 @@ public class Billing {
     public static double computeBill(double price) {
         double tax=8.0 / 100 * price;
         double total = price + tax;
+        System.out.println(total);
         return total;
     }
     public static double computeBill(double price,int quantity) {
@@ -11,14 +12,16 @@ public class Billing {
         double total_price=price*quantity;
         double tax= 8.0 / 100 * total_price;
         double total = total_price +tax;
+        System.out.println(total);
         return total;
     }
     public static double computeBill(double price,int quantity,double coupon) {
 
         double total_price=price*quantity;
-        double final_price=total_price*coupon/100;
+        double final_price=total_price-total_price*coupon/100;
         double tax= 8.0 / 100 * final_price;
         double total = final_price +tax;
+        System.out.println(total);
         return total;
     }
 
@@ -26,6 +29,9 @@ public class Billing {
 
     public static void main(String[] args) {
         // write your code here
+        computeBill(50);
+        computeBill(50,5);
+        computeBill(50,5,20);
         
     }
 }
