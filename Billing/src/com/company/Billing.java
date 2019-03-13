8d458739e3b6faa0c6b015bb7f1b6b1fea017da8
GharehzadehAli <gharehzadehali@gmail.com>
2019-03-13 14:03:11 +0330

    adding computeBill() method which returns price of the books considering coupon

--- Billing/src/com/company/Billing.java
+++ Billing/src/com/company/Billing.java
@@ -10,12 +10,22 @@ public class Billing {
 
         double total_price=price*quantity;
         double tax= 8.0 / 100 * total_price;
-        double total = total_price +tax; 
+        double total = total_price +tax;
+        return total;
+    }
+    public static double computeBill(double price,int quantity,double coupon) {
+
+        double total_price=price*quantity;
+        double final_price=total_price*coupon/100;
+        double tax= 8.0 / 100 * final_price;
+        double total = final_price +tax;
         return total;
     }
 
 
+
     public static void main(String[] args) {
         // write your code here
+        
     }
 }
