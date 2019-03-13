9d11ec7159880e1eca97977969d320445bd69f57
GharehzadehAli <gharehzadehali@gmail.com>
2019-03-13 15:18:14 +0330

    Last version of solution for question 4

--- Patient/src/com/company/TestPatient.java
+++ Patient/src/com/company/TestPatient.java
@@ -4,5 +4,16 @@ public class TestPatient {
 
     public static void main(String[] args) {
 	// write your code here
+        Patient patient=new Patient();
+        System.out.println(patient.getAge());
+        System.out.println(patient.getBloodData().getBloodType());
+        System.out.println(patient.getBloodData().getRhFactor());
+        System.out.println(patient.getID());
+        BloodData bloodData=new BloodData("A","-");
+        Patient patient1=new Patient(1,20,bloodData);
+        System.out.println(patient1.getID());
+        System.out.println(patient1.getAge());
+        System.out.println(patient1.getBloodData().getRhFactor());
+        System.out.println(patient1.getBloodData().getBloodType());
     }
 }
